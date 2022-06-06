a316892eb836f00138a32cb3eda3cbe48a06c01a
nerabab <106933563+nerabab@users.noreply.github.com>
2022-06-06 10:11:32 +0800

    Create _worker.js

---
+++ _worker.js
@@ -0,0 +1,18 @@
+export default {
+  async fetch(request, env) {
+    const day1 = 'nerabab.herokuapp.com'
+    const day2 = 'nerabac.herokuapp.com'
+    let url = new URL(request.url);
+    if (url.pathname.startsWith('/')) {
+      let day = new Date()
+      if (day.getDay() % 2) {
+        url.hostname = day1
+      } else {
+        url.hostname = day2
+      }
+      let new_request = new Request(url, request);
+      return fetch(new_request);
+    }
+    return env.ASSETS.fetch(request);
+  },
+};
